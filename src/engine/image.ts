6d7d600f1054e6e9eba05732829d8eb2c48325e7
ForkΨKILLET <fork_killet@outlook.com>
2024-11-18 03:21:39 +0800

    fix: loading a same image multiple times might cause a crash

--- src/engine/image.ts
+++ src/engine/image.ts
@@ -3,24 +3,27 @@ export interface ImageManager {
     loadImage: (src: string) => Promise<HTMLImageElement>
 }
 
+export const isImageLoaded = (img: HTMLImageElement) => img.complete && img.naturalWidth !== 0
+
 export const useImageManager = (): ImageManager => {
     const imgs: Record<string, HTMLImageElement> = {}
+    const loadingImgs: Record<string, Promise<HTMLImageElement>> = {}
 
     return {
         imgs,
         loadImage: async (src: string) => {
-            if (imgs[src]) return imgs[src]
+            if (src in imgs) return imgs[src]
+            if (src in loadingImgs) return loadingImgs[src]
+
             const img = new Image()
             img.src = src
-            imgs[src] = img
-            await new Promise((res, rej) => {
-                img.onload = res
+            return loadingImgs[src] = new Promise((res, rej) => {
+                img.onload = () => res(img)
                 img.onerror = event => {
                     rej(new Error(`Failed to load image: ${ src }`))
                     console.error(`Failed to load image: ${ src }\n%o`, event)
                 }
             })
-            return img
         },
     }
 }
