e3bcd74eaea12f7274757e256c319292dc719b3a
ForkΨKILLET <fork_killet@outlook.com>
2024-11-28 02:00:40 +0800

    feat(about): add about

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_BUILD_TIME?: string
+    readonly VITE_BUILD_ENV?: string
+    readonly VITE_LAST_COMMIT?: string
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
+
